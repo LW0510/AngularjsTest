36201df976956cd7b79ebe5897e63f92d4e6a049
LW0510 <2581992919@qq.com>
2019-05-22 17:29:35 +0800

    added test7

--- test2/js/test7.js
+++ test2/js/test7.js
@@ -2,6 +2,12 @@
 angular.module('MyApp', [])
     .controller('MyController', ['$scope', function ($scope) {
 
+        $scope.stu={
+            id:1001,
+            name:'张三'
+        }
+
+        $scope.hobbies = ['eat','play','sleep'];
     }
     ]);
 
